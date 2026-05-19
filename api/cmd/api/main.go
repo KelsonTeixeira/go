7f0ae8220091bd012cfd1b664f229f54b4314127
kelsonteixeira <txkelson@gmail.com>
2026-05-19 02:39:25 -0300

    add tools

--- api/cmd/api/main.go
+++ api/cmd/api/main.go
@@ -4,13 +4,16 @@ import (
 	_ "fmt"
 	_ "net/http"
 
+	"github.com/go-chi/chi"
 	_ "github.com/go-chi/chi"
 
-	_ "github.com/avukadin/goapi/internal/handlers"
+	"github.com/kelsonteixeira/go/internal/handlers"
 
-	_ "github.com/sirupsen/logrus"
+	log "github.com/sirupsen/logrus"
 )
 
 func main() {
-
+	log.SetReportCaller(true)
+	var r *chi.Mux = chi.NewRouter()
+	handlers.Handler(r)
 }
---
+++ internal/middlwware/go.mod
@@ -0,0 +1,3 @@
+module github.com/kelsonteixeira/go/internal/middleware
+
+go 1.26.3
