6e7f8c5212e3328a79377538900e847d9a0cdb7d
kelsonteixeira <txkelson@gmail.com>
2026-05-24 04:42:26 -0300

    array study

--- basics/main.go
+++ basics/main.go
@@ -1,14 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func main() {
-	fmt.Println("This is the begining.")
-	anotherFileTest()
-	variable()
-	maps()
-	loop()
-	conditional()
-	types()
-	errorHandling()
+	// fmt.Println("This is the begining.")
+	// anotherFileTest()
+	// variable()
+	// maps()
+	// loop()
+	// conditional()
+	// types()
+	// errorHandling()
+
+	a := []string{"d", "c", "a", "b"}
+	sort.Strings(a)
+	fmt.Println(a)
 }
